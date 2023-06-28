1a259970148753256e7c8a75ad75d7fb2b0595cd
Nikola Mitić <101733519+miticn@users.noreply.github.com>
2023-06-28 02:29:18 +0200

    Auth works

--- Authentication/configuration.py
+++ Authentication/configuration.py
@@ -1,7 +1,14 @@
 from datetime import timedelta
+from os import environ
 
 class Configuration:
-    SQLALCHEMY_DATABASE_URI   = "mysql://root:root@database:3306/users"
+    #127.0.0.1
+    #CHECK ENVIRONMENT VARIABLES
+    if "DOCKER_INSTANCE" in environ:
+        DATABASE_URL = "database"
+    else:
+        DATABASE_URL = "127.0.0.1"
+    SQLALCHEMY_DATABASE_URI   = f"mysql://root:root@{DATABASE_URL}:3306/users"
     JWT_SECRET_KEY            = "JWT_SECRET_KEY"
     JWT_ACCESS_TOKEN_EXPIRES  = timedelta ( minutes = 15 );
     JWT_REFRESH_TOKEN_EXPIRES = timedelta ( days = 30 );
