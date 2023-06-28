f8e7d9f2cc8f70931fa7dd1cbdcb0c4ba76faeb0
Nikola Mitić <101733519+miticn@users.noreply.github.com>
2023-06-28 14:14:19 +0200

    Login

--- Authentication/main.py
+++ Authentication/main.py
@@ -4,12 +4,13 @@ from models import User;
 from models import database;
 
 from configuration import Configuration;
-
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import JWTManager
 
 app = Flask (__name__);
 app.config.from_object (Configuration);
 database.init_app ( app )
-
+jwt = JWTManager ( app )
 def register (role):
     fields = ["forename", "surname", "email", "password"];
     #return 404 if field is missing with message field in json
@@ -46,7 +47,31 @@ def register_courier ( ):
 
 @app.route ("/login", methods=["POST"])
 def login ( ):
-    return "login";
+    fields = ["email", "password"];
+    #return 404 if field is missing with message field in json
+    for field in fields:
+        if not field in request.json or request.json[field] == "":
+            return {"message": "Field " + field + " is missing"}, 404;
+
+    #check if email is valid
+    if not "@" in request.json["email"] or not "." in request.json["email"]:
+        return {"message": "Invalid email."}, 404;
+
+    #check credentials from database
+    user = User.query.filter_by (email = request.json["email"], password = request.json["password"]).first ( );
+    if user == None:
+        return {"message": "Invalid credentials."}, 404;
+
+    #create token
+    claims = {
+            "forename": user.forename,
+            "surname": user.surname,
+            "roles": user.role
+    }
+    access_token  = create_access_token ( identity = user.email, additional_claims = claims )
+
+    return {"accessToken":access_token},200;
+    
 
 @app.route ("/delete", methods=["POST"])
 def delete ( ):
