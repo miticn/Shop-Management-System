a2d622e8d42d5245079bf69a1d0dca25b48f73a9
Nikola Mitić <101733519+miticn@users.noreply.github.com>
2023-09-14 14:10:48 +0200

    Order validation

--- Store/customer.py
+++ Store/customer.py
@@ -38,5 +38,44 @@ def search ( claims):
         'products':[product.to_json() for product in products]
         }, 200;
 
+
+@app.route ("/order", methods=["POST"])
+@authentication_required
+@customer_required
+def order ( claims):
+    request_data = request.get_json ( );
+    if not "requests" in request_data:
+        return {"message": "Field requests is missing."}, 400;
+    requests = request_data["requests"];
+
+    for req_i in range(len(requests)):
+        if not "id" in requests[req_i]:
+            return {"message": "Product id is missing for request number "+str(req_i)+"."}, 400;
+        if not "quantity" in requests[req_i]:
+            return {"message": "Product quantity is missing for request number "+str(req_i)+"."}, 400;
+        try:
+            product_id = int(requests[req_i]["id"]);
+        except ValueError:
+            return {"message": "Invalid product id for request number "+str(req_i)+"."}, 400;
+        if product_id < 0:
+            return {"message": "Invalid product id for request number "+str(req_i)+"."}, 400;
+        try:
+            quantity = int(requests[req_i]["quantity"]);
+        except ValueError:
+            return {"message": "Invalid product quantity for request number "+str(req_i)+"."}, 400;
+        if quantity < 0:
+            return {"message": "Invalid product quantity for request number "+str(req_i)+"."}, 400;
+        product = Product.query.filter_by(id = product_id).first ( );
+        if product == None:
+            return {"message": "Invalid product for request number "+str(req_i)+"."}, 400;
+
+    return "", 200;
+
+@app.route ("/status", methods=["GET"])
+@authentication_required
+@customer_required
+def status ( claims):
+    return "", 200;
+
 if ( __name__ == "__main__" ):
     app.run ( host="0.0.0.0", debug = True, port = 5002 )
